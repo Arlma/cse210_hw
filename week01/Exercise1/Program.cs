706a81cfffbc55c8cad17e9acef422fb947d5b7e
Me@1 <makumu@byupathway.edu>
2025-09-08 13:09:54 +0300

    Finished Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Ask for first name
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
